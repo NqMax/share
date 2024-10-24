1e63a25f67e497fe7caa874d87b3da7a9e9a9f58
Max <111196993+NqMax@users.noreply.github.com>
2024-10-23 23:21:43 -0600

    feat: add route handlers to main

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
 import { Hono } from "@hono/hono";
+import domains from "@/routes/domains.ts";
+import urls from "@/routes/urls.ts";
+import { showRoutes } from "@hono/hono/dev";
+import { logger } from "@hono/hono/logger";
 
 const app = new Hono();
+app.use(logger());
+
+app.route("/", domains);
+app.route("/", urls);
+
+showRoutes(app, {
+  verbose: true,
+});
 
 Deno.serve(app.fetch);
