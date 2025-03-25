3d66f04faf13724301a56e69d44db7fc2e02849a
Max <111196993+NqMax@users.noreply.github.com>
2025-03-24 21:21:30 -0600

    feat: move into monorepo

--- services/url-service.ts
+++
@@ -1,7 +0,0 @@
-import { sql } from "@/db/db.ts";
-import { urlSchema } from "@/schemas/index.ts";
-import type { z } from "zod";
-
-export async function createUrl(url: z.infer<typeof urlSchema>) {
-  return await sql`INSERT INTO urls ${sql(url)} RETURNING *`;
-}
